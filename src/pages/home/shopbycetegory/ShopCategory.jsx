04714e3880fb9561a26b7c437aa44b0e0861f7c7
somratsam <somratsam2@gmail.com>
2023-05-21 02:58:19 +0600

    create modal for singale page

--- src/pages/home/shopbycetegory/ShopCategory.jsx
+++ src/pages/home/shopbycetegory/ShopCategory.jsx
@@ -1,14 +1,19 @@
+
+
+
+
+
 import React, { useEffect, useState } from 'react';
-import { Tab, Nav, Card, Container, Row, Col } from 'react-bootstrap';
+import { Tab, Nav, Card, Container, Row, Col, Button, Modal } from 'react-bootstrap';
 import { FaStar } from 'react-icons/fa';
 import BestSeller from '../BestSeller';
 import NewReleases from '../NewReleases';
-
 const ShopCategory = () => {
   const [activeTab, setActiveTab] = useState('1');
   const [categories, setCategories] = useState([]);
   const [bestSellerData, setBestSellerData] = useState([]);
   const [newReleasesData, setNewReleasesData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -31,26 +36,30 @@ const ShopCategory = () => {
   };
 
   const handleRatingChange = (rating) => {
-    // Handle rating change here
     console.log('Selected rating:', rating);
   };
 
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    <Container className="mx-auto">
+    <Container className="mx-auto my-5">
       <h2 className="text-center mb-4">Shop by Category</h2>
       <Tab.Container activeKey={activeTab} onSelect={handleTabSelect}>
         <Nav justify variant="tabs" className="mb-3">
           {categories.map((category) => (
             <Nav.Item key={category._id}>
               <Nav.Link
-                className={`fw-bold ${activeTab === category._id ? 'bg-warning' : ''}`}
-                eventKey={category.id}
-                style={{
-                  color: activeTab === category._id ? '#ff6600' : '#000'
-                }}
-              >
-                {category.name}
-              </Nav.Link>
+  className={`fw-bold ${activeTab === category.id ? 'bg-warning' : ''}`}
+  eventKey={category.id}
+  style={{
+    color: activeTab === category.id ? '#ff6600' : '#000'
+  }}
+>
+  {category.name}
+</Nav.Link>
+
             </Nav.Item>
           ))}
         </Nav>
@@ -86,6 +95,16 @@ const ShopCategory = () => {
                                 </span>
                               </div>
                             </Card.Body>
+                            <div>
+                              <Button
+                                variant="light"
+                                className="rounded-5 text-dark fw-bold"
+                                style={{ backgroundColor: '#FF5722' }}
+                                onClick={handleModal}
+                              >
+                                View Details
+                              </Button>
+                            </div>
                           </Card>
                         </Col>
                       ))}
@@ -99,6 +118,44 @@ const ShopCategory = () => {
       </Tab.Container>
       <BestSeller bestSellerData={bestSellerData} />
       <NewReleases newReleasesData={newReleasesData} />
+
+      <Modal className="w-100 h-100" show={showModal} onHide={handleModal}>
+  <Modal.Header closeButton>
+    <Modal.Title>Details</Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+    {categories.map((category) => {
+      if (category.id === activeTab) {
+        return category.subcategories.map((subcategory) => (
+          <div key={subcategory.name}>
+            <h4 className=" mt-4">{subcategory.name}</h4>
+            {subcategory.toys.map((toy, index) => (
+              <div key={index} className="d-flex justify-content-between">
+                <div className="w-25">
+                  <img className="w-100" src={toy.image} alt="" />
+                </div>
+                <div className="w-75">
+                  <h6>Toy {index + 1}</h6>
+                  <p>Price: ${toy.price}</p>
+                  {/* Add any other relevant data here */}
+                </div>
+              </div>
+            ))}
+          </div>
+        ));
+      }
+      return null;
+    })}
+  </Modal.Body>
+  <Modal.Footer>
+    <Button variant="secondary" onClick={handleModal}>
+      Close
+    </Button>
+    <Button variant="primary">Save Changes</Button>
+  </Modal.Footer>
+</Modal>
+
+
     </Container>
   );
 };
